1dad793f2b6439c485e12ecac71d7c3f5e330aae
Piyush Kumar Jaiswal <piyushkumarj48@gmail.com>
2025-09-03 19:02:17 +0530

    updated upstream

--- frontend/public/src/api.js
+++ frontend/public/src/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: "/api", // relative, Vercel will route it to FastAPI
 });
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
 
-export default API;
+export default api;
